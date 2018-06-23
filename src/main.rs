8b329a3aea24a5c29f65be6ee096acc443c62bec
Asari Mitsuteru <asari.mtr@gmail.com>
2018-06-23 17:22:34 +0900

    Simple response

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,27 @@
-extern crate time;
-
+use std::thread;
 use std::io;
 use std::io::{Write, BufRead};
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+
+fn handle_client(stream: TcpStream) {
+    let mut stream = io::BufReader::new(stream);
+
+    loop {
+        let mut line = String::new();
+        println!("{}", line);
+        match stream.read_line(&mut line) {
+            Ok(2) => {
+                let mut stream = stream.get_mut();
+                writeln!(stream, "HTTP/1.1 200 OK").unwrap();
+                writeln!(stream, "Content-Type: text/html; charset=UTF-8").unwrap();
+                writeln!(stream, "Content-Length: {}", 0).unwrap();
+                break;
+            },
+            Err(err) => panic!("error during receive a line: {}", err),
+            _ => print!("{}", line.trim_right_matches("\r\n"))
+        }
+    }
+}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
@@ -10,19 +29,9 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                let mut stream = io::BufReader::new(stream);
-
-                loop {
-                    let mut line = String::new();
-                    match stream.read_line(&mut line) {
-                        Ok(0) => {
-                            println!("end");
-                            break;
-                        },
-                        Err(err) => panic!("error during receive a line: {}", err),
-                        _ => print!("{}", line)
-                    }
-                }
+                thread::spawn(move || {
+                    handle_client(stream)
+                });
             }
             Err(e) => {
                 println!("Connection fail!");
