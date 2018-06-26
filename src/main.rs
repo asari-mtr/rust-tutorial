1b5d1d2da1f24afd5816651fd72c2205650856ef
Asari Mitsuteru <mitsuteru.asari@supership.jp>
2018-06-26 18:19:16 +0900

    Change to receive only 'GET /'

--- src/main.rs
+++ src/main.rs
@@ -15,29 +15,28 @@ fn handle_client(stream: TcpStream, addr: SocketAddr) {
             let version = iter.next().unwrap();
             println!("method: {}", method);
             println!("ip: {}", addr.ip());
-            get_operation(stream);
+            println!("{}", request_line);
+            if method == "GET" && uri == "/" {
+                let message = "<html><head><title>Hello</title></head><body>Hello World!</body></html>";
+                loop {
+                    let mut line = String::new();
+                    match stream.read_line(&mut line) {
+                        Ok(2) => {
+                            let mut stream = stream.get_mut();
+                            writeln!(stream, "HTTP/1.1 200 OK").unwrap();
+                            writeln!(stream, "Content-Type: text/html; charset=UTF-8").unwrap();
+                            writeln!(stream, "Content-Length: {}", message.len()).unwrap();
+                            writeln!(stream).unwrap();
+                            writeln!(stream, "{}", message).unwrap();
+                            break;
+                        },
+                        Err(err) => panic!("error during receive a line: {}", err),
+                        _ => println!("{}", line.trim_right_matches("\r\n"))
+                    }
+                }
+            }
         },
         Err(err) => panic!("error during receive a line: {}", err),
-    }
-}
-
-fn get_operation(stream: BufRead) {
-    let message = "<html><head><title>Hello</title></head><body>Hello World!</body></html>";
-    loop {
-        let mut line = String::new();
-        match stream.read_line(&mut line) {
-            Ok(2) => {
-                let mut stream = stream.get_mut();
-                writeln!(stream, "HTTP/1.1 200 OK").unwrap();
-                writeln!(stream, "Content-Type: text/html; charset=UTF-8").unwrap();
-                writeln!(stream, "Content-Length: {}", message.len()).unwrap();
-                writeln!(stream).unwrap();
-                writeln!(stream, "{}", message).unwrap();
-                break;
-            },
-            Err(err) => panic!("error during receive a line: {}", err),
-            _ => println!("{}", line.trim_right_matches("\r\n"))
-        }
     }
 }
 
