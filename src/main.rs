b2d2e608061896e8054daf4ebedb25ee1aac1595
Asari Mitsuteru <asari.mtr@gmail.com>
2018-07-28 00:06:09 +0900

    Optimize import & Move method

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,15 @@
-use std::thread;
-use std::fs;
-use std::io::{Write, BufWriter};
-use std::net::{TcpListener, TcpStream, SocketAddr};
-use std::path::Path;
-
 extern crate flate2;
-use flate2::Compression;
-use flate2::write::GzEncoder;
-
-mod request;
-use request::Request;
-
-mod response;
-use response::*;
-
-mod status_code;
-use status_code::StatusCode;
 
 mod constants;
+mod request;
+mod response;
+mod status_code;
+
 use constants::*;
-
-fn public_path(path: &str) -> String {
-    let mut base = String::from(ROOT_DIR);
-
-    if path == "/" {
-        base.push_str("/index.html")
-    } else {
-        base.push_str(path)
-    }
-
-    base
-}
+use request::Request;
+use response::*;
+use std::net::{TcpListener, TcpStream, SocketAddr};
+use std::thread;
 
 fn dispatch(stream: TcpStream, _addr: SocketAddr) {
     let request = Request::new(&stream);
@@ -39,67 +18,6 @@ fn dispatch(stream: TcpStream, _addr: SocketAddr) {
     if request.method == GET {
         response(request, stream);
     }
-}
-
-fn response(request: Request, stream: TcpStream) {
-    let public_path = public_path(&request.uri);
-
-    let (public_path, status) = valid_file_path(&public_path);
-
-    let data = read_data(request, &public_path);
-
-    let headers = create_response_headers(status, &public_path, &data);
-
-    write_response(stream, headers, data);
-}
-
-fn valid_file_path(public_path: &str) -> (String, StatusCode) {
-    if Path::new(&public_path).exists() {
-        (public_path.to_string(), StatusCode::Ok)
-    } else {
-        ("public/404.html".to_string(), StatusCode::NotFound)
-    }
-}
-
-fn read_data(request: Request, public_path: &str) -> Vec<u8> {
-    let data = fs::read(&public_path).expect("Unable to read file");
-    match request.headers.get(ACCEPT_ENCODING) {
-        Some(keys) => {
-            // It needs to use a more accurate match method.
-            if keys.contains(GZIP) {
-                let mut e = GzEncoder::new(Vec::new(), Compression::default());
-
-                e.write(&data).unwrap();
-                match e.finish() {
-                    Ok(v) => v,
-                    Err(e) => panic!("fail encode to zip: {}", e)
-                }
-            } else {
-                data
-            }
-        },
-        None => data
-    }
-}
-
-fn create_response_headers(status: StatusCode, public_path: &str, data: &Vec<u8>) -> ResponseHeaders {
-    let mut headers = ResponseHeaders::new();
-    headers.write_http_status_line(status);
-    headers.write_content_type(&public_path);
-    headers.write_content_length(data.len());
-    headers.write_content_encoding();
-    headers
-}
-
-fn write_response(stream: TcpStream, headers: ResponseHeaders, data: Vec<u8>) {
-    let mut stream = BufWriter::new(stream);
-
-    for header in headers {
-        writeln!(stream, "{}", header).unwrap();
-    }
-    writeln!(stream).unwrap();
-
-    stream.write(&data).unwrap();
 }
 
 fn main() {
--- src/response.rs
+++ src/response.rs
@@ -1,6 +1,14 @@
 extern crate mime_guess;
 
+use constants::*;
+use flate2::Compression;
+use flate2::write::GzEncoder;
+use request::Request;
 use status_code::StatusCode;
+use std::fs;
+use std::io::{Write, BufWriter};
+use std::net::{TcpStream};
+use std::path::Path;
 
 pub type ResponseHeaders = Vec<String>;
 
@@ -30,3 +38,75 @@ impl WriteResponseHeaders for ResponseHeaders {
     }
 }
 
+pub fn response(request: Request, stream: TcpStream) {
+    let public_path = public_path(&request.uri);
+
+    let (public_path, status) = valid_file_path(&public_path);
+
+    let data = read_data(request, &public_path);
+
+    let headers = create_response_headers(status, &public_path, &data);
+
+    write_response(stream, headers, data);
+}
+
+fn public_path(path: &str) -> String {
+    let mut base = String::from(ROOT_DIR);
+
+    if path == "/" {
+        base.push_str("/index.html")
+    } else {
+        base.push_str(path)
+    }
+
+    base
+}
+
+fn valid_file_path(public_path: &str) -> (String, StatusCode) {
+    if Path::new(&public_path).exists() {
+        (public_path.to_string(), StatusCode::Ok)
+    } else {
+        ("public/404.html".to_string(), StatusCode::NotFound)
+    }
+}
+
+fn read_data(request: Request, public_path: &str) -> Vec<u8> {
+    let data = fs::read(&public_path).expect("Unable to read file");
+    match request.headers.get(ACCEPT_ENCODING) {
+        Some(keys) => {
+            // It needs to use a more accurate match method.
+            if keys.contains(GZIP) {
+                let mut e = GzEncoder::new(Vec::new(), Compression::default());
+
+                e.write(&data).unwrap();
+                match e.finish() {
+                    Ok(v) => v,
+                    Err(e) => panic!("fail encode to zip: {}", e)
+                }
+            } else {
+                data
+            }
+        },
+        None => data
+    }
+}
+
+fn create_response_headers(status: StatusCode, public_path: &str, data: &Vec<u8>) -> ResponseHeaders {
+    let mut headers = ResponseHeaders::new();
+    headers.write_http_status_line(status);
+    headers.write_content_type(&public_path);
+    headers.write_content_length(data.len());
+    headers.write_content_encoding();
+    headers
+}
+
+fn write_response(stream: TcpStream, headers: ResponseHeaders, data: Vec<u8>) {
+    let mut stream = BufWriter::new(stream);
+
+    for header in headers {
+        writeln!(stream, "{}", header).unwrap();
+    }
+    writeln!(stream).unwrap();
+
+    stream.write(&data).unwrap();
+}
